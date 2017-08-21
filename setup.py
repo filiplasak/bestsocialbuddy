9dd6acd58903e7508aaf93e2e5fda623d0cd378b
Filip Lasak <filip@lasak.com.pl>
2017-08-21 19:45:47 +0200

    Add google analitycs id

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='best-social-buddy',
-    version='1.1.4',
+    version='1.1.5',
     long_description=__doc__,
     packages=['app'],
     include_package_data=True,
